286efa55f3ab87c314f50bbaa5705a34ff270420
ometman <revomet@gmail.com>
2023-06-03 06:41:32 +0000

    impletement api connection to add and generate score records

--- src/modules/addScore.js
+++ src/modules/addScore.js
@@ -1,18 +1,15 @@
-import ScoresClass from './scoresClass.js';
-import displayScores from './showScores.js';
+import displayScores from './showNewScore.js';
+import postNewScore from './postScore.js';
 
 const addNewScore = () => {
-  const newName = document.querySelector('#name-input');
-  const newScore = document.querySelector('#score-input');
   const theForm = document.querySelector('#game-form');
-  theForm.addEventListener('click', (e) => {
+  theForm.addEventListener('click', async (e) => {
     e.preventDefault();
-    const newScoreList = new ScoresClass();
-    const playname = newName.value;
-    const playscore = newScore.value;
+    const playname = document.querySelector('#name-input').value;
+    const playscore = document.querySelector('#score-input').value;
     if (playname !== '' && playscore !== '') {
-      newScoreList.addScore(playname, playscore);
-      displayScores();
+      await postNewScore({ user: playname, score: playscore });
+      displayScores({ user: playname, score: playscore });
       theForm.reset();
     }
   });
